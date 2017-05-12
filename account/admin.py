2cff148ddcc527388f4b5e8e3b058224f805798e
Shrikrishna singh <krishnasingh.ss30@gmail.com>
2017-05-12 14:17:44 +0530

    costomize admin

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from account.models import Userprofile,SocratesSearch,Following,newspaper
 
+class UserprofileAdmin(admin.ModelAdmin):
+    list_display = ('user','location')
+    search_fields = ('location',)
 admin.site.register(Userprofile)
 admin.site.register(SocratesSearch)
 admin.site.register(Following)
