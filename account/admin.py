71912601660b02da0d47a3c93e6f52d459e28a2f
CodeVINCI <myc.abhi96@gmail.com>
2017-05-12 12:34:12 +0530

    Now you can follow certain newspaper

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from account.models import Userprofile,SocratesSearch,Following
+from account.models import Userprofile,SocratesSearch,Following,newspaper
 
 admin.site.register(Userprofile)
 admin.site.register(SocratesSearch)
 admin.site.register(Following)
+admin.site.register(newspaper)
 
--- account/models.py
+++ account/models.py
@@ -19,7 +19,14 @@ class Userprofile(models.Model):
     def __str__(self):
         return self.user.username
 
+class newspaper(models.Model):
+    name= models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering=('name',)
 
 
 class SocratesSearch(models.Model):
@@ -27,15 +34,16 @@ class SocratesSearch(models.Model):
     search=models.CharField(max_length=200,default='',blank=False)
     searchdate=models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.user.username
 
 
 class Following(models.Model):
     users=models.ManyToManyField(User,related_name='people')
+    newspaper=models.ManyToManyField(newspaper)
     current_user=models.ForeignKey(User, related_name='owner', null=True)
 
-    def __str__(self):
+    def __unicode__(self):
         return str(self.current_user)
 
     @classmethod
@@ -51,3 +59,7 @@ class Following(models.Model):
             current_user=current_user
         )
         follower.users.remove(new_following)
+
+
+
+
