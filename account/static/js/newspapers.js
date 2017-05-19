491043ef45a7670f13950e584e1b8fd7d331a74e
CodeVINCI <myc.abhi96@gmail.com>
2017-05-20 00:00:19 +0530

    now like and dislike buttons are giving urls on clicking make request to those urls to like and dislike

--- account/static/js/newspapers.js
+++ account/static/js/newspapers.js
@@ -11,4 +11,19 @@ var ur = ("/account/newspapers/").concat(target);
 window.location.href = ur;
 });
 
+$('.box').on('click', "button.social-like", function(event)
+{
+event.preventDefault();
+var id =$(this).next('meta').data().postid;
+var ur =("/post/like/").concat(id);
+alert(ur);
+});
+
+$('.box').on('click', "button.social-dislike", function(event)
+{
+event.preventDefault();
+var id =$(this).prev('meta').data().postid;
+var ur =("/post/dislike/").concat(id);
+alert(ur);
+});
 });
--- home/static/js/home.js
+++ home/static/js/home.js
@@ -6,10 +6,27 @@ $(function() {
 });
 $('div.select_filters').on('click', ".btn.btn-secondary", function(event)
 {
+event.preventDefault();
 var target= document.getElementById("filters").value;
 var ur = ("/home/").concat(target);
 window.location.href = ur;
 
+});
+
+$('#wrap').on('click', "button.social-like", function(event)
+{
+event.preventDefault();
+var id =$(this).next('meta').data().postid;
+var ur =("/post/like/").concat(id);
+alert(ur);
+});
+
+$('#wrap').on('click', "button.social-dislike", function(event)
+{
+event.preventDefault();
+var id =$(this).prev('meta').data().postid;
+var ur =("/post/dislike/").concat(id);
+alert(ur);
 });
 
 });
