0cea6eb27f9c25329918f37631a6bf46e328646a
CodeVINCI <myc.abhi96@gmail.com>
2017-05-29 16:46:59 +0530

    before krishna commits add buttons

--- home/TheHindu.py
+++ home/TheHindu.py
@@ -48,6 +48,8 @@ def get_news(URL):
 
                     # Get the filename from the url, used for saving later
                     file_name = image_url.split('/')[-1]+".jpg"
+                    if len(file_name)>50:
+                        file_name=file_name[:50]+."jpg"
 
                     # Create a temporary file
                     lf = tempfile.NamedTemporaryFile()
