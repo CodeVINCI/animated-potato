98a10b0a45607bc631f5ac5f8e861905cab755b0
CodeVINCI <myc.abhi96@gmail.com>
2017-05-31 21:28:51 +0530

    United nations robot working and delivering the news

--- home/tasks.py
+++ home/tasks.py
@@ -4,6 +4,7 @@ from celery.task.schedules import  crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 from home.TheHindu import get_news
+from home.unnewsstream import ScrapeUN
 
 logger = get_task_logger(__name__)
 
@@ -13,6 +14,7 @@ logger = get_task_logger(__name__)
     ignore_results=True)
 def scrape_Thehindu_task():
     URLs=["https://newsapi.org/v1/articles?source=business-insider&sortBy=latest&apiKey=7b761e381bcc40ca88311d8ef360da90"," https://newsapi.org/v1/articles?source=breitbart-news&sortBy=latest&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=bloomberg&sortBy=top&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=bild&sortBy=top&apiKey=7b761e381bcc40ca88311d8ef360da90"," https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=ars-technica&sortBy=latest&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=al-jazeera-english&sortBy=latest&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=abc-news-au&sortBy=top&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=the-hindu&sortBy=latest&apiKey=7b761e381bcc40ca88311d8ef360da90","https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=7b761e381bcc40ca88311d8ef360da90"]
+    ScrapeUN()
     for URL in URLs:
         get_news(URL)
         logger.info("Scraped The Hindu")
---
+++ home/unnewsstream.py
@@ -0,0 +1,97 @@
+import urllib
+import json
+import ssl
+import bs4
+from bs4 import BeautifulSoup
+import re
+import time
+from home.models import Post
+import requests
+import tempfile
+from django.core import files
+from django.utils import timezone
+
+
+def ScrapeUN():
+    url= "http://unnewsstream.org/"
+    context = ssl._create_unverified_context()
+    html_base=urllib.urlopen(url,context=context)
+    soup_base=BeautifulSoup(html_base,"html.parser")
+    newslinks=soup_base.findAll("div",{"class":"news"})
+    articles=[]
+    for link in newslinks:
+        urlToImage=link.find("div",{"class":"media-box-image"})
+        urlToImage=urlToImage.findAll('div')[0]['data-thumbnail']
+
+        title=link.find("div",{"class":"media-box-title"}).text+"."
+        title=" ".join(title.split())
+
+        description=link.find("div",{"class":"media-box-text"}).text
+        description=" ".join(description.split())
+
+        url=link.find("div",{"class":"media-box-title"}).find('a')['href']
+
+        date=link.find("div",{"class":"media-box-date"})
+        date=date.text
+        date=date.strip()
+
+        article={"author":"un-news-stream","title":title,"description":description,"url":url,"urlToImage":urlToImage,"publishedAt":date}
+        articles.append(article)
+    response={"source":"unnewsstream.org","articles":articles}
+    response=json.dumps(response)
+
+    data=json.loads(response)
+    source= data["source"].encode('utf-8')
+    articles=data["articles"]
+    for article in articles:
+        storedarticles=Post.objects.filter(headline=article["title"].encode('utf-8'))
+        if len(storedarticles)==0:
+                articlesource=source
+                if article["author"]==None:
+                    author="Anonymous"
+                else:
+                    author=article["author"].encode('utf-8')
+                headline=article["title"].encode('utf-8')
+                story=article["description"].encode('utf-8')
+                url=article["url"].encode('utf-8')
+                if article["publishedAt"]==None:
+                    date= str(timezone.now())
+                    date=date[:10]
+                else:
+                    date= str(timezone.now())
+                    date=date[:10]
+                if not(article['urlToImage']==None):
+                    image_url=article["urlToImage"].encode('utf-8')
+                    request = requests.get(image_url, stream=True)
+
+                    # Was the request OK?
+                    if request.status_code != requests.codes.ok:
+                    # Nope, error handling, skip file etc etc etc
+                        continue
+
+                    # Get the filename from the url, used for saving later
+                    file_name = image_url.split('/')[-1]+".jpg"
+                    if len(file_name)>50:
+                        file_name=file_name[:50]+".jpg"
+
+                    # Create a temporary file
+                    lf = tempfile.NamedTemporaryFile()
+
+                    # Read the streamed image in sections
+                    for block in request.iter_content(1024 * 8):
+
+                        # If no more file then stop
+                        if not block:
+                            break
+
+                        # Write image block to temporary file
+                        lf.write(block)
+
+                    # Create the model you want to save the image to
+                    a=Post(source=articlesource,author=author,headline=headline,story=story,link=image_url,date=date,pageurl=url)
+
+                    # Save the temporary image to the model#
+                    # This saves the model so be sure that is it valid
+                    a.image.save(file_name, files.File(lf))
+
+
