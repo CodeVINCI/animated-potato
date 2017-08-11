e21e96d1ad96641ba78b98cccfd31a20d27f555d
CodeVINCI <myc.abhi96@gmail.com>
2017-08-12 01:36:10 +0530

    made carousel in compare page

--- account/static/js/compare.js
+++ account/static/js/compare.js
@@ -1,14 +1,18 @@
-/*$(document).ready(function()
+$(document).ready(function()
 {
+
+$('.carousel').carousel({ interval: 5000 });
+
 $(document).on('click','.comparetitle',function(event)
 {
 var target = $($(this).attr('data-target'));
 
 var imgbox = target.find('.imgbox');
-var da = target.find('.container');
+var da = target.find('.conatiner');
 var urllist = [da.attr('data-img1'),da.attr('data-img2'),da.attr('data-img3')];
-var
+
+
 
 });
 
-});*/
+});
