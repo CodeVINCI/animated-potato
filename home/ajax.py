bb862cde036f3106774968a876045d0675187488
CodeVINCI <myc.abhi96@gmail.com>
2017-09-03 19:59:11 +0530

    made all buttons in compare

--- home/ajax.py
+++ home/ajax.py
@@ -1,4 +1,5 @@
 from account.models import Notification,Compare
+from .models import Post
 from django.http import JsonResponse
 
 def seennotification(request,pk):
@@ -14,3 +15,21 @@ def updatecompare(request,pk):
     desc = request.GET.get('description',"")
     Compare.objects.filter(pk=pk).update(title=title,description=desc)
     return JsonResponse({"message":1})
+
+def publishcompare(request,pk):
+    Compare.objects.filter(pk=pk).update(published=1)
+    return JsonResponse({'message':1})
+
+def unpublishcompare(request,pk):
+    Compare.objects.filter(pk=pk).update(published=0)
+    return JsonResponse({'message':1})
+def deletecompare(request,pk):
+    Compare.objects.get(pk=pk).delete()
+    return JsonResponse({'message':1})
+
+def removefromcompare(request,pk):
+    com=Compare.objects.get(pk=pk)
+    p=request.GET.get('post','')
+    post = Post.objects.get(pk=p)
+    com.posts.remove(post)
+    return JsonResponse({'message':1})
