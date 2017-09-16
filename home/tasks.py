01f7cb0811da2b9ce369de0eea6500fd0b91b59a
CodeVINCI <myc.abhi96@gmail.com>
2017-09-16 18:45:41 +0530

    made delete posts task everyday

--- home/tasks.py
+++ home/tasks.py
@@ -7,6 +7,10 @@ from home.TheHindu import get_news
 from home.unnewsstream import ScrapeUN
 from home.fashion import Scrape_bussiness_of_fashion
 from home.Hindinews import Scrape_hindustan,Scrap_amarujala,Scrap_dainik_bhaskar,Scrape_dainik_jagran
+from account.models import Following,Compare
+from home.models import Post
+from datetime import datetime,timedelta
+import os
 
 logger = get_task_logger(__name__)
 
@@ -36,3 +40,28 @@ def scrape_Thehindu_task():
         get_news(URL)
         source=URL.strip()[32:].split('&')[0]
         logger.info("Scraped "+source)
+
+@periodic_task(
+    run_every=(crontab(minute='*/1440')),
+    name= "delete_Post",
+    ignore_results=True)
+def delete_Post():
+    all_exclusions = []
+    all_compares=Compare.objects.all()
+    for compare in all_compares:
+        all_exclusions= all_exclusions+list(Compare.posts.all())
+    all_followings= Following.objects.all()
+    for following in all_followings:
+        all_exclusions= all_exclusions+list(following.post.all())
+    all_pks = []
+    for exclusion in all_exclusions:
+        all_pks.append(exclusion.pk)
+    date_today = datetime.today()
+    d = str(datetime.today()-timedelta(days=14))
+    all_deletes = Post.objects.exclude(pk__in = all_pks).exclude(date__range=[d,date_today])
+    for post in all_deletes:
+        url = post.image.url
+        pa = os.getcwd()+'/website'+url
+        os.remove(pa)
+        post.delete()
+
